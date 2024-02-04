8cc1cc389a06169707a8cf45da92fc054f52aa8b
ArinaJur <66705803+ArinaJur@users.noreply.github.com>
2024-02-04 12:45:49 -0500

    Base test refactoring, isOnHomePage logic added (#2)
    * first commit
    
    * Framework created
    
    * Base test refactoring, isOnHomePage logic added
    
    ---------
    
    Co-authored-by: ArinaJur <pw.rep.auto.24@gmail.com>

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,95 @@
+package tests;
+
+import com.microsoft.playwright.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import utils.TestData;
+
+import static utils.TestData.BASE_URL;
+import static utils.TestData.HOME_END_POINT;
+
+abstract class BaseTest {
+
+    private final Playwright playwright = Playwright.create();
+    private final Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+            .setHeadless(false).setSlowMo(1500));
+    private BrowserContext context;
+    private Page page;
+
+    @BeforeSuite
+    void checkIfPlaywrightCreatedAndBrowserLaunched() {
+        if (playwright != null) {
+            System.out.println("Playwright created");
+        } else {
+            System.out.println("FATAL: Playwright is NOT created.");
+            System.exit(1);
+        }
+
+        if(browser.isConnected()) {
+            System.out.println("Browser " + browser.browserType().name() + " is connected.");
+        } else {
+            System.out.println("FATAL: Browser is NOT connected.");
+            System.exit(1);
+        }
+    }
+
+    @BeforeMethod
+    void createContextAndPage() {
+        context = browser.newContext();
+        System.out.println("Context created");
+
+        page = context.newPage();
+        System.out.println("Page created");
+
+        System.out.println("Start test");
+
+        getPage().navigate(BASE_URL);
+
+        if(isOnHomePage()) {
+            System.out.println("Base url is opened and content is not empty.");
+        } else {
+            System.out.println("ERROR: Base url is NOT opened OR content is EMPTY.");
+        }
+    }
+
+    @AfterMethod
+    void closeContext() {
+        if (page != null) {
+            page.close();
+            System.out.println("Page closed");
+        }
+        if (context != null) {
+            context.close();
+            System.out.println("Context closed");
+        }
+    }
+
+    @AfterSuite
+    void closeBrowserAndPlaywright() {
+        if (browser != null) {
+            browser.close();
+
+        }
+        if (playwright != null) {
+            playwright.close();
+            System.out.println("Playwright closed");
+        }
+    }
+
+    private boolean isOnHomePage() {
+        getPage().waitForLoadState();
+
+        return getPage().url().equals(BASE_URL + HOME_END_POINT) && !page.content().isEmpty();
+    }
+
+    Page getPage() {
+        return page;
+    }
+
+    protected boolean getIsOnHomePage() {
+
+        return isOnHomePage();
+    }
+}
---
+++ src/test/java/tests/NavigationTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.TestData;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static utils.TestData.BASE_URL;
+
+public final class NavigationTest extends BaseTest {
+
+    @Test
+    public void testBaseUrlLanding() {
+        getPage().navigate(BASE_URL);
+
+        assertThat(getPage()).hasURL(BASE_URL + TestData.HOME_END_POINT);
+    }
+
+    @Test
+    public void testForHimMenuNavigatesToForHimPage() {
+        if (getIsOnHomePage()) {
+
+            getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(TestData.FOR_HIM).setExact(true)).click();
+
+            assertThat(getPage()).hasURL(BASE_URL + TestData.FOR_HIM_END_POINT);
+        } else {
+            Assert.fail();
+        }
+    }
+
+}
