dd5267f5121230a313ce41e7f53e765f728a693c
ArinaJur <pw.rep.auto.24@gmail.com>
2024-02-11 12:38:48 -0500

    imports optimized

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,7 +1,6 @@
 package tests;
 
 import com.microsoft.playwright.*;
-import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
